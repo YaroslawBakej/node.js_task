fabbf9e8076fb96544b5161b4f702683b585379f
unknown <jarobak1995@gmail.com>
2022-12-16 23:35:39 +0300

    commit

--- 003/index.js
+++ 003/index.js
@@ -4,9 +4,9 @@ const port = 8000
 
 app.listen(port, () => console.log(`ready steady go on port ${port}`))
 
-// ставим в первую очередь index.js
-// далее создаем файл app.js, controller.js, sevices.js, storage.json
-// в app.js создаем переменную const express=require('express')
+// 1. ставим в первую очередь index.js
+// 2. далее создаем файлы app.js, controller.js, sevices.js, storage.json
+// 3. в app.js создаем переменную const express=require('express')
 // const app=express()
 // импортируем парсер const bodyParser = require('body-Parser'), app.use(bodyParser.json())
 // потом импортируем контроллер const cotroller=require('адрес контроллера')
@@ -15,13 +15,13 @@ app.listen(port, () => console.log(`ready steady go on port ${port}`))
 // app.use((error, req, res, next) => {
 //     res.status(500).send(error.message);
 // });
-// далее переходим в сервисы
+// 4. далее переходим в сервисы
 // const fs=require('fs')
 // const {readFileSync, writeFileSync}=require('fs')
 // импортируем хранилище const storage='путь к джсон файлу'
 // создаем функции для запроса, обновления, удаления джсон файла
 // далее экспортируем наши функции module.exports={getUser....}
-//переходим в контроллер
+//5. переходим в контроллер
 // в контроллере импортируем экспресс
 // const express=require('express')
 //импортируем функции из сервисов 
